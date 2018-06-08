6121d1a9bd53e078f7ab1922adb87f52f046950b
Jacob Cornwell <jacob.p.cornwell@gmail.com>
2018-06-07 22:50:01 -0500

    Set up beginning structure for test suite

--- 003-day/main.c
+++ 003-day/main.c
@@ -1,18 +1,30 @@
 
-
 #include "ast.h"
 #include "parser.h"
 
-int main(int argc, char **argv) {
-    char *source = "12*34 + 45/56 + ~~25 ** 2 ** 3";
+void set_input_stream(char *source) {
     stream = source;
+}
 
+void generate_s_expression() {
     AST_Node *tree;
 
-    // tree = parse_expression();
     tree = parse_expression();
     print_ast(tree);
     printf("\n");
+}
+
+void run_test_suite() {
+    set_input_stream("1 + 2");
+    generate_s_expression();
+
+    set_input_stream("1 + 2 * 3");
+    generate_s_expression();
+}
+
+int main(int argc, char **argv) {
+    run_test_suite();
+    printf("Tests completed successfully\n");
 
     return 0;
 }
